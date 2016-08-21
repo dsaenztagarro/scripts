1da1a8896075dd85cf0466adbbf95dda64abd2e2
David Saenz <david.saenz.tagarro@gmail.com>
2016-08-21 09:22:06 +0200

    Improves script for jekyll

--- Scripts/local.dsaenz.booklog.sh
+++ Scripts/local.dsaenz.booklog.sh
@@ -1,13 +1,13 @@
 #!/bin/bash --login
-BASE_DIR="/Users/$USER/Projects/booklog"
+PROJECT_NAME="booklog"
+JEKYLL_PORT=4000
 
-JEKYLL_SERVE_CMD="$BASE_DIR/jekyll serve --watch --drafts --port 4000"
+BASE_DIR="/Users/$USER/Projects/$PROJECT_NAME"
+JEKYLL_SERVE_CMD="jekyll serve --watch --drafts --port $JEKYLL_PORT"
 
 # Stop jekyll
-ps ax | grep "$JEKYLL_SERVE_CMD" | grep -v grep | awk '{ print $1 }'
-
-# Rebuild
-$BASE_DIR/jekyll build
+ps ax | grep "$JEKYLL_SERVE_CMD" | grep -v grep | awk '{ print $1 }' | xargs kill -9
 
 # Start jekyll
-$JEKYLL_SERVE_CMD
+cd $BASE_DIR
+eval $JEKYLL_SERVE_CMD
