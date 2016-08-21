69b0fbbd51ad81a896ced71f452890237a0fc647
David Saenz <david.saenz.tagarro@gmail.com>
2016-08-21 08:36:45 +0200

    Rewritten jekyll build

--- Scripts/local.dsaenz.booklog.sh
+++ Scripts/local.dsaenz.booklog.sh
@@ -1,4 +1,13 @@
 #!/bin/bash --login
-cd /Users/$USER/Projects/booklog
-jekyll build
-jekyll serve --watch --drafts --port 4000
+BASE_DIR="/Users/$USER/Projects/booklog"
+
+JEKYLL_SERVE_CMD="$BASE_DIR/jekyll serve --watch --drafts --port 4000"
+
+# Stop jekyll
+ps ax | grep "$JEKYLL_SERVE_CMD" | grep -v grep | awk '{ print $1 }'
+
+# Rebuild
+$BASE_DIR/jekyll build
+
+# Start jekyll
+$JEKYLL_SERVE_CMD
